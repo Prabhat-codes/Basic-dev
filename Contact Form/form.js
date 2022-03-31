7100f7e09f613bc392e456bfacfb61e0d6602a38
Prabhat <prabhat.namdharani@gmail.com>
2022-03-31 12:58:26 +0530

    Contact Form

---
+++ Contact Form/form.js
@@ -0,0 +1,22 @@
+function acceptdata(){
+    a=document.getElementById("name").value;
+    alert(a);
+    console.log(a);
+    b=document.getElementById("email").value;
+    console.log(b);
+    alert(b);
+    c=document.getElementById('phone').value;
+    console.log(c);
+    alert(c);
+    console.log("hello world");
+    alert("hello world");
+    console.log("Helloworld");
+
+}
+
+function clear(){
+    console.log("HEre");
+    document.getElementById("name").value="";
+    document.getElementById("email").value="";
+    document.getElementById("phone").value="";
+}
